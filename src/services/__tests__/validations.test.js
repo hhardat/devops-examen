c762405e536175ff5e1d0f75d81eb9f103eb7157
Hugo <hugohardat@gmail.com>
2023-01-20 10:08:13 +0100

    ran code fix

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -22,25 +22,25 @@ describe("validation test suites - isValid", () => {
     test("should return false as the label is undefined", () => {
         const result = isValid();
         expect(result).toBe(false);
-    })
+    });
     test("should return false as the label is empty", () => {
         const result = isValid("");
         expect(result).toBe(false);
-    })
+    });
     test("should return false as the label is not eight char or more", () => {
         const result = isValid("azer");
         expect(result).toBe(false);
-    })
+    });
     test("should return false as the label does not contain a special char", () => {
         const result = isValid("azertyui");
         expect(result).toBe(false);
-    })
+    });
     test("should return false as the label does not contain a number", () => {
         const result = isValid("azruytruioyz=");
         expect(result).toBe(false);
-    })
+    });
     test("should return true as the label is valid", () => {
         const result = isValid("aazzzzee!8è!");
         expect(result).toBe(true);
-    })
-})
+    });
+});
